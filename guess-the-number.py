cbade23abd22443e8f0e6079f83564b11dea0113
CoderTan <tech.tan06@gmail.com>
2026-06-17 23:19:58 +0400

    Guess-the-number game program

---
+++ guess-the-number.py
@@ -0,0 +1,15 @@
+#Guess the number program (1-100) - TS
+import random
+
+number = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess == number:
+        print("Correct! You guessed it!")
+        break
+    elif guess < number:
+        print("Too low, try again.")
+    else:
+        print("Too high, try again.")
