8c7ef9662e91bbe6ce977908010df5de4e88c6af
CoderTan <tech.tan06@gmail.com>
2026-06-17 23:34:15 +0400

    Rock-paper-pencil-scissors game

--- Rock-paper-scissors game.py
+++ Rock-paper-scissors game.py
@@ -1,6 +1,6 @@
-#Rock, paper and scissors game coding - Tanya Singh
+#Rock, paper, pencil and scissors game coding - TS
 import random
-user_choice_number=int(input("Enter the number corresponding to your choice (1/2/3) :"))
+user_choice_number=int(input("Enter the number corresponding to your choice (1/2/3/4) :"))
 
 #Convert number to choice
 if user_choice_number == 1:
@@ -8,22 +8,26 @@ if user_choice_number == 1:
 elif user_choice_number == 2:
     user_choice = 'Paper'
 elif user_choice_number == 3:
+    user_choice = 'Pencil'
+elif user_choice_number == 4:
     user_choice = 'Scissors'
 else:
     print("Invalid choice!")
     exit()
 
 #Computer randomly selects a choice
-choices = ['Rock','Paper','Scissors']
+choices = ['Rock','Paper','Pencil','Scissors']
 computer_choice = random.choice(choices)
 print(f"Computer chose : {computer_choice}")
 
 #Determine the winner
 if user_choice == computer_choice:
     print("It's a tie!")
-elif (user_choice == 'Rock' and computer_choice == 'Scissors') or \
-     (user_choice == 'Paper' and computer_choice == 'Rock') or \
-     (user_choice == 'Scissors' and computer_choice == 'Paper'):
+elif (user == "rock" and computer in ["scissors", "pencil"]) or \
+     (user == "paper" and computer in ["rock", "pencil"]) or \
+     (user == "pencil" and computer == "paper") or \
+     (user == "scissors" and computer in ["paper", "pencil"]):
     print("You win! Congrats!")
 else:
     print("Computer wins! Try next time!")
+#End of program
